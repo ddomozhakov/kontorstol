4f8958a24d0f7c6a8a0f1064491f202afab83628
Denis Domozhakov <dwe@MacBook-Pro-DWE.local>
2024-12-29 17:17:54 +0300

    Revert "getItemsAmount no database"
    This reverts commit 18af314567d253456cfabaab5f15804da1f62d7b.

--- lager/src/main/java/ru/dwe/kontorstol/lager/controller/LagerController.java
+++
@@ -1,8 +0,0 @@
-package ru.dwe.kontorstol.lager.controller;
-
-import ru.dwe.kontorstol.lager.model.ItemsAmountRq;
-import ru.dwe.kontorstol.lager.model.ItemsAmountRs;
-
-public interface LagerController {
-    ItemsAmountRs getItemsAmount(ItemsAmountRq rq);
-}
--- lager/src/main/java/ru/dwe/kontorstol/lager/service/LagerService.java
+++
@@ -1,8 +0,0 @@
-package ru.dwe.kontorstol.lager.service;
-
-import ru.dwe.kontorstol.lager.model.ItemsAmountRq;
-import ru.dwe.kontorstol.lager.model.ItemsAmountRs;
-
-public interface LagerService {
-    ItemsAmountRs getItemsAmount(ItemsAmountRq rq);
-}
